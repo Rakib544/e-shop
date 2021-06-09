60c28b0c1dfc1ad96e10066d3215f2e4bd0787e9
Rakib544 <md.rakib10122003@gmail.com>
2021-06-09 13:04:54 +0600

    some design implemented done

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
+import NavBar from '../components/Navbar/Navbar';
 import { loadDataFromLocalStorage } from '../redux/actions/cartActions';
 
 
-const Cart:React.FC = () => {
+const Cart: React.FC = () => {
+
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(loadDataFromLocalStorage())
@@ -11,11 +13,24 @@ const Cart:React.FC = () => {
 
     const products = useSelector((state: RootStateOrAny) => state.cart.cartItems)
     return (
-        <div>
-            {
-                products?.map(product => <li>{product.name}</li>)
-            }
-        </div>
+        <>
+            <NavBar />
+            <div className="px-10 sm:px-2 md:px-8 lg:px-10 py-10">
+                <div className="flex flex-wrap -m-4 justify-between">
+                    <div className="p-2 sm:w-1/2 md:w-1/2 order-2 sm:order-2 lg:order-1">
+                        {
+                            products?.map(product => <li>{product.name}</li>)
+                        }
+                    </div>
+                    <div className="p-2 w-full sm:w-full lg:w-1/4 bg-gray-100 order-1 sm:order-1 lg:order-2">
+                        <h2 className="text-2xl">Sub-Total (0) Items</h2>
+                        <p className="text-lg mt-4 font-medium">$656</p>
+                        <button className="bg-gray-800 text-white w-full py-3 mt-4">Proceed To Checkout</button>
+                    </div>
+                </div>
+
+            </div>
+        </>
     );
 };
 
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
+import NavBar from '../../components/Navbar/Navbar';
 import Preloader from '../../components/Preloader/Preloader';
 import { addToCart } from '../../redux/actions/cartActions';
 import { getProductDetails } from '../../redux/actions/productActions';
@@ -8,7 +9,7 @@ import { getProductDetails } from '../../redux/actions/productActions';
 const ProductDetails: React.FC = () => {
     const router = useRouter()
     const dispatch = useDispatch()
-    
+
     useEffect(() => {
         dispatch(getProductDetails(router.query.id))
     }, [])
@@ -17,15 +18,16 @@ const ProductDetails: React.FC = () => {
         console.log(state)
         return state.getProductDetails
     });
-    
+
     const { product, loading, error } = state;
     return (
         <div>
+            <NavBar />
             {
                 loading
                     ? <Preloader />
                     : (
-                        <div className="container-lg px-8 py-14 mx-auto">
+                        <div className="h:screen container-lg px-8 py-14 mx-auto">
                             <div className="flex flex-wrap -m-4">
                                 <div className="p-2 sm:w-1/2 lg:w-1/3" >
                                     <img className="w-full h-full" src={product?.img} alt={product.name} />
@@ -43,7 +45,7 @@ const ProductDetails: React.FC = () => {
                                         </div>
                                         <div className="flex justify-between">
                                             <p>Status</p>
-                                            <p>{product.stock ? 'In Stock': 'Out Of Stock'}</p>
+                                            <p>{product.stock ? 'In Stock' : 'Out Of Stock'}</p>
                                         </div>
                                         <button onClick={() => dispatch(addToCart(product))} className="bg-gray-900 text-white w-full py-3 mt-10 hover:bg-opacity-80 rounded">ADD TO CART</button>
                                     </div>
