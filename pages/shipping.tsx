75fc5b6d69de832feb51e3ffd9bf20896bdfb3ea
Rakib544 <md.rakib10122003@gmail.com>
2021-06-10 23:33:54 +0600

    shipping page data collect done

--- pages/shipping.tsx
+++ pages/shipping.tsx
@@ -1,21 +1,31 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import NavBar from "../components/Navbar/Navbar";
+import { useForm } from "react-hook-form";
 
 const Shipping = () => {
     const router = useRouter();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+        router.push('/payment')
+    };
     return (
         <>
             <NavBar />
             <div className="w-1/2 mx-auto mt-8">
                 <h1 className="text-4xl">SHIPPING</h1>
-                <form className="mt-4">
+                <form
+                    className="mt-4"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
                     <div className="my-3">
                         <label htmlFor="address">Address</label>
                         <input
                             className="block w-full px-4 py-3 border-0 focus:outline-none bg-gray-50 mt-2"
                             type="text"
                             placeholder="Enter Address"
+                            name="address"
+                            ref={register({required: true})}
                         />
                     </div>
                     <div className="my-3">
@@ -24,6 +34,8 @@ const Shipping = () => {
                             className="block w-full px-4 py-3 border-0 focus:outline-none bg-gray-50 mt-2"
                             type="text"
                             placeholder="Enter City"
+                            name="city"
+                            ref={register({required: true})}
                         />
                     </div>
                     <div className="my-3">
@@ -32,6 +44,8 @@ const Shipping = () => {
                             className="block w-full px-4 py-3 border-0 focus:outline-none bg-gray-50 mt-2"
                             type="text"
                             placeholder="Enter Postal Code"
+                            name="postalCode"
+                            ref={register({required: true})}
                         />
                     </div>
                     <div className="my-3">
@@ -40,12 +54,13 @@ const Shipping = () => {
                             className="block w-full px-4 py-3 border-0 focus:outline-none bg-gray-50 mt-2"
                             type="text"
                             placeholder="Enter Country"
+                            name="country"
+                            ref={register({required: true})}
                         />
                     </div>
                     <button
                         type="submit"
                         className="px-5 py-3 bg-gray-800 text-white my-4"
-                        onClick={() => router.push('/payment')}
                     >
                         CONTINUE
                     </button>
